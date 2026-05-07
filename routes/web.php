dbe253640deb8185497ae0448fc6890b665b0f48
softiondeveloper <bryanlebenn@gmail.com>
2026-05-07 20:47:30 +0700

    feat : tambah pesanan page and detail page

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ Route::get('/', fn() => redirect()->route('pesanan.index'));
 
 Route::get('/test', [TestController::class, 'index']);
 
-Route::get('/pesanan',   [PesananController::class,  'index'])->name('pesanan.index');
+//pesanan
+Route::get('/pesanan',          [PesananController::class,  'index'])->name('pesanan.index');
+Route::get('/pesanan/create',   [PesananController::class,  'create'])->name('pesanan.create');
+Route::post('/pesanan',         [PesananController::class,  'store'])->name('pesanan.store');
+Route::get('/pesanan/{id}',     [PesananController::class,  'show'])->name('pesanan.show');
+
 Route::get('/pelanggan', [PelangganController::class, 'index'])->name('pelanggan.index');
-Route::get('/produk',    [ProdukController::class,   'index'])->name('produk.index');
+Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');

6e3c329d0fd7c2c7ba2104279479009a589f9e62
softiondeveloper <bryanlebenn@gmail.com>
2026-05-16 11:59:15 +0700

    feat : connect to database and integrate provinsi and kabupaten list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TOPLA – @yield('title', 'Dashboard')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
 </head>
 <body class="bg-gray-50 antialiased">
     <div class="flex h-screen overflow-hidden">
